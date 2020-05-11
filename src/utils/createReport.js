696976b9e9e90d36aebca09dfdaac84046929dec
maisa12 <nikamaisa@gmail.com>
2020-05-12 00:15:47 +0400

    delete comments

--- src/utils/createReport.js
+++ src/utils/createReport.js
@@ -3,15 +3,12 @@ const mainScript = require('./mainScript');
 const Employee = require('../models/employee');
 const createXlsx  = require('./createExcell');
 let createReport = () =>{
-  
     const dataDb =  Employee.find({},{__v: false, _id:false}).sort({employeeNo: 1}).lean().exec();
     const dataUp = read();
     Promise.all([dataDb, dataUp])
             .then(response=>mainScript(response[0],  response[1]))
             .then(report=>createXlsx(report))
             .catch(e=>console.log(e))
-    //let report = await mainScript(dataDb,  dataUp);
-   // createXlsx(report)
 }
 
 module.exports = createReport
